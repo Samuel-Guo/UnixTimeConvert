9d66f7d1ac8cd48cf26b5e7ae6a223a125044969
Guo Sentao <nowandthen@qq.com>
2019-04-19 15:26:00 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnixTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            var epoch = (DateTime.Now.ToUniversalTime().Ticks - 621355968000000000) / 10000000;
+            textBox1.Text = epoch.ToString();
+        
+
+        }
+        /// <summary>  
+        /// Unix时间戳转为C#格式时间  
+        /// </summary>  
+        /// <param name="timeStamp">Unix时间戳格式,例如1482115779</param>  
+        /// <returns>C#格式时间</returns>  
+        public static DateTime GetTime(string timeStamp)
+        {
+            DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long lTime = long.Parse(timeStamp + "0000000");
+            TimeSpan toNow = new TimeSpan(lTime);
+            return dtStart.Add(toNow);
+        }
+
+
+        /// <summary>  
+        /// DateTime时间格式转换为Unix时间戳格式  
+        /// </summary>  
+        /// <param name="time"> DateTime时间格式</param>  
+        /// <returns>Unix时间戳格式</returns>  
+        public static long ConvertDateTimeInt(DateTime time)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (long)(time - startTime).TotalSeconds;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TStamp_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                tToTime.Text = GetTime(tStamp.Text).ToString();
+
+            }
+            catch
+            { }
+
+        }
+
+        private void TTime_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                tToStamp.Text = ConvertDateTimeInt(Convert.ToDateTime(tTime.Text)).ToString();
+
+            }
+            catch
+            { }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+    }
+}
